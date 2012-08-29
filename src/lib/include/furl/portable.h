cbd49a8d2f7b6ed053de1151019a5da8fb510046
Sebastien Tricaud <sebastien.tricaud@gmail.com>
2012-08-29 08:53:49 +0200

    Add a portable file containing memrchr for MACOSX

---
+++ src/lib/include/furl/portable.h
@@ -0,0 +1,33 @@
+/*
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+ * Version 2, December 2004 
+ *
+ * Copyright (C) 2012 Sebastien Tricaud <sebastien@honeynet.org> 
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified 
+ * copies of this license document, and changing it is allowed as long 
+ * as the name is changed. 
+ *
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+ * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
+#ifndef _FURL_PORTABLE_H_
+#define _FURL_PORTABLE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef MACOS
+void *memrchr(const void *s, int c, size_t n);
+#endif /* MACOS */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* _FURL_PORTABLE_H_ */
