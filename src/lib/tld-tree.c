06322097fc0e7b9def44352f7023c2917df284ce
Sebastien Tricaud <sebastien@honeynet.org>
2013-06-05 09:36:35 -0300

    Add stderr to the fprintf

--- src/lib/tld-tree.c
+++ src/lib/tld-tree.c
@@ -134,7 +134,7 @@ static int faup_tld_tree_add_node(TLDNode **Tree, char *TLD, int tld_len)
 	TLDNode *pNode;
 
 	if (!Tree) {
-		fprintf("%s Tree does not exists!\n", __FUNCTION__);
+		fprintf(stderr, "%s Tree does not exists!\n", __FUNCTION__);
 		return -1;
 	}
 
