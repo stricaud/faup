f59a204f8d5c18c7f70589191dc2ae6248ea46bb
c-x <cedric.lrx@gmail.com>
2014-05-21 15:49:08 +0200

    Fix #40 - python bindings & test script update

--- src/lib/bindings/python/test.py
+++ src/lib/bindings/python/test.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
 
 from pyfaup.faup import Faup
+import re
 import sys
 import codecs
 import binascii
+
+# dynamically list all Faup's methods
+methods = []
+for m in dir( Faup ):
+	if re.search("^get_", m) :
+		methods.append( m )
+methods.remove("get_version")
+
+
+# run
+# run
+if len(sys.argv) != 2 :
+    print "%s <file containing 1 url per line>" % sys.argv[0]
+    sys.exit(0)
 
 f = Faup()
 file_urls=codecs.open(sys.argv[1],'r','ascii',errors='ignore')
@@ -13,5 +28,10 @@ for url in urls:
     url=url.replace('\n','')
     print("URL:[%s]" % (url))
     f.decode(url)
-    print("-----> Extracted TLD:%s" % f.get_tld())
+#    print("-----> Extracted TLD:%s" % f.get_tld())
+#    print("-----> Extracted TLD:%s" % f.get_domain_without_tld())
+
+    for m in methods:
+	fct = getattr(f, m)
+	print "\t%s : %s" % (re.sub("^get_", "", m), fct())
 
