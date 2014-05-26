9f6c4399884ce3857cdb6170739295c1884d16b0
Sebastien Tricaud <sebastien@honeynet.org>
2014-05-26 21:44:50 +0200

    Add a test tool to faup for base64

---
+++ src/tests/base64.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <faup/base64.h>
+
+int main(int argc, char **argv)
+{
+  unsigned char *data_unbase64;
+  char          *data_base64;
+  int            outlen;
+
+  if (argc < 2) {
+    fprintf(stderr, "No arguments given. Please provide an url to test\n");
+    return -1;
+  }
+
+  data_base64 = base64( (const void*) argv[1], strlen(argv[1]), &outlen);
+  printf("base64:-->%s<--\n", data_base64);
+
+  data_unbase64 = unbase64((const char *)data_base64, outlen, &outlen);
+  printf("unbase64:-->%s<--\n", data_unbase64);
+
+
+}
