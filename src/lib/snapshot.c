bd2ac58397fd7226e526c91daee288143d9701be
Sebastien Tricaud <sebastien@honeynet.org>
2016-03-16 07:00:16 -0700

    Insert stuff sorted

--- src/lib/snapshot.c
+++ src/lib/snapshot.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #define __USE_XOPEN_EXTENDED // for strdup() BS glibc now needs
 #include <string.h>
+#define __USE_GNU // qsort_r()
 #include <stdlib.h>
 #include <time.h>
 
@@ -33,6 +34,12 @@ void faup_snapshot_value_count_debug(faup_snapshot_value_count_t *vc)
 void faup_snapshot_item_debug(faup_snapshot_item_t *item)
 {
   size_t counter;
+
+  if (!item) {
+    printf("** item empty, cannot debug!\n");
+    return;
+  }
+  
   printf("** \titem\n");
   printf("** \tkey:%s\n", item->key);
   printf("** \tlength:%ld\n", item->length);
@@ -52,6 +59,39 @@ void faup_snapshot_debug(faup_snapshot_t *snapshot)
     faup_snapshot_item_debug(snapshot->items[counter]);
   }
 }
+
+static int compare_items(const void *p1, const void *p2)
+{
+  faup_snapshot_item_t ** item1 = (faup_snapshot_item_t **)p1;
+  faup_snapshot_item_t ** item2 = (faup_snapshot_item_t **)p2;
+
+  printf("compare %s with %s\n", (*item1)->key, (*item2)->key);
+
+  return strcmp((*item1)->key, (*item2)->key);
+}
+
+static int compare_search(const void *key, const void *member)
+{
+  faup_snapshot_item_t *k = (faup_snapshot_item_t *)key;
+  faup_snapshot_item_t *m = (faup_snapshot_item_t *)member;
+  
+  /* faup_snapshot_item_t *k = (faup_snapshot_item_t *)key;   */
+  /* faup_snapshot_item_t **m = (faup_snapshot_item_t **)member; */
+
+  
+  return strcmp(k->key, m->key);
+
+  /* printf("key:%s\n", (*m)->key); */
+}
+
+static int compare_simple(const void *p1, const void *p2)
+{
+  const faup_snapshot_item_t *item1 = (faup_snapshot_item_t *)p1;
+  const faup_snapshot_item_t *item2 = (faup_snapshot_item_t *)p2;
+
+  return strcmp(item1->key, item2->key);
+}
+
 
 faup_snapshot_value_count_t *faup_snapshot_value_count_new(void)
 {
@@ -166,6 +206,11 @@ int faup_snapshot_value_count_append(faup_snapshot_item_t *item, char *value)
 {
   faup_snapshot_value_count_t *vc;
 
+  if (!item) {
+    fprintf(stderr, "Cannot append value '%s' to item\n", value);
+    return -1;
+  }
+  
   vc = faup_snapshot_value_count_get(item, value);
   if (!vc) {
     item->value_count = realloc(item->value_count, sizeof(faup_snapshot_value_count_t *) * (item->length+1));
@@ -214,27 +259,31 @@ void faup_snapshot_item_free(faup_snapshot_item_t *item)
 
 faup_snapshot_item_t *faup_snapshot_item_get(faup_snapshot_t *snapshot, char *item_name)
 {
+  faup_snapshot_item_t *res, key;
   size_t counter;
-
+  
   if (!snapshot) {
     fprintf(stderr, "Snapshot is NULL!\n");
     return NULL;
   }
 
+  /* key.key = item_name; */
+  /* res = bsearch(&key, snapshot->items, snapshot->length, sizeof(faup_snapshot_item_t *), compare_simple); */
+  /* faup_snapshot_item_debug(res); */
   for (counter = 0; counter < snapshot->length; counter++) {
     if (!strcmp(snapshot->items[counter]->key, item_name)) {
       return snapshot->items[counter];
     }
   }
-
-  /* fprintf(stderr, "No such item found:%s\n", item_name); */
+  
   return NULL;
 }
 
 int faup_snapshot_item_append(faup_snapshot_t *snapshot, char *item_name)
 {
   faup_snapshot_item_t *item;
-
+  void *arg;
+  
   item = faup_snapshot_item_get(snapshot, item_name);
   if (!item) {
     snapshot->items = realloc(snapshot->items, sizeof(faup_snapshot_item_t *) * (snapshot->length + 1));
@@ -245,6 +294,10 @@ int faup_snapshot_item_append(faup_snapshot_t *snapshot, char *item_name)
     snapshot->items[snapshot->length] = faup_snapshot_item_new();
     snapshot->items[snapshot->length]->key = strdup(item_name);
     snapshot->length++;
+
+    /* qsort_r(snapshot->items, snapshot->length, sizeof(faup_snapshot_item_t *), compare_items, arg); */
+    qsort(snapshot->items, snapshot->length, sizeof(faup_snapshot_item_t *), compare_simple);
+
   }
   
   return 0;
@@ -259,7 +312,9 @@ void faup_snapshot_free(faup_snapshot_t *snapshot)
   }
   free(snapshot->items);
   for (counter = 0; counter < snapshot->length; counter++) {
-    free(snapshot->items[counter]->key);
+    /* if (snapshot->items[counter]) { */
+    /*   free(snapshot->items[counter]->key); */
+    /* } */
   }
   
   free(snapshot);
@@ -288,11 +343,16 @@ int faup_snapshot_append(faup_snapshot_t *snapshot, char *key, char *value)
 
   faup_snapshot_item_append(snapshot, key);
   item = faup_snapshot_item_get(snapshot, key);  
-  faup_snapshot_value_count_append(item, value);
-
+  if (item) {
+    faup_snapshot_value_count_append(item, value);
+  } else {
+    fprintf(stderr, "Item does not exists. Cannot append value!\n");
+  }
+  
   return 0;
 }
 
+// FIXME: Remove this useless function
 int faup_snapshot_append_item(faup_snapshot_t *snapshot, char *item_name, faup_snapshot_item_t *item)
 {
   if (!snapshot) {
@@ -307,7 +367,6 @@ int faup_snapshot_append_item(faup_snapshot_t *snapshot, char *item_name, faup_s
   }
 
   snapshot->items[snapshot->length] = item;
-  snapshot->items[snapshot->length]->key = strdup(item_name);
   snapshot->length++;
   
   return 0;
