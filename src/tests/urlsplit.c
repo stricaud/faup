28c08d01ba623d975f46f7c51c26632ad085481c
Sebastien Tricaud <str@detecteam.com>
2024-12-10 11:00:01 +0100

    Test all splits

--- src/tests/urlsplit.c
+++ src/tests/urlsplit.c
@@ -17,12 +17,57 @@ int main(int argc, char **argv)
 	pos = faup_get_scheme_pos(fh);
 	size = faup_get_scheme_size(fh);
 	if (size > 0) {
-		char *out = malloc(size + 1);
-		memset(out, '\0', size+1);
-		memcpy(out, instr, size);
-
-		printf("The decoded scheme is '%s'\n", out);
-		free(out);
+		printf("scheme:%.*s\n", (int)size, instr + pos);
+	}
+	pos = faup_get_credential_pos(fh);
+	size = faup_get_credential_size(fh);
+	if (size > 0) {
+		printf("credential:%.*s\n", (int)size, instr + pos);
+	}
+	pos = faup_get_subdomain_pos(fh);
+	size = faup_get_subdomain_size(fh);
+	if (size > 0) {
+		printf("subdomain:%.*s\n", (int)size, instr + pos);
+	}
+	pos = faup_get_domain_pos(fh);
+	size = faup_get_domain_size(fh);
+	if (size > 0) {
+		printf("domain:%.*s\n", (int)size, instr + pos);
+	}
+	pos = faup_get_domain_without_tld_pos(fh);
+	size = faup_get_domain_without_tld_size(fh);
+	if (size > 0) {
+		printf("domain without tld:%.*s\n", (int)size, instr + pos);
+	}
+	pos = faup_get_host_pos(fh);
+	size = faup_get_host_size(fh);
+	if (size > 0) {
+		printf("host:%.*s\n", (int)size, instr + pos);
+	}
+	pos = faup_get_tld_pos(fh);
+	size = faup_get_tld_size(fh);
+	if (size > 0) {
+		printf("tld:%.*s\n", (int)size, instr + pos);
+	}
+	pos = faup_get_port_pos(fh);
+	size = faup_get_port_size(fh);
+	if (size > 0) {
+		printf("port:%.*s\n", (int)size, instr + pos);
+	}
+	pos = faup_get_resource_path_pos(fh);
+	size = faup_get_resource_path_size(fh);
+	if (size > 0) {
+		printf("resource path:%.*s\n", (int)size, instr + pos);
+	}
+	pos = faup_get_query_string_pos(fh);
+	size = faup_get_query_string_size(fh);
+	if (size > 0) {
+		printf("query string:%.*s\n", (int)size, instr + pos);
+	}
+	pos = faup_get_fragment_pos(fh);
+	size = faup_get_fragment_size(fh);
+	if (size > 0) {
+		printf("fragment:%.*s\n", (int)size, instr + pos);
 	}
 
 	faup_terminate(fh);
